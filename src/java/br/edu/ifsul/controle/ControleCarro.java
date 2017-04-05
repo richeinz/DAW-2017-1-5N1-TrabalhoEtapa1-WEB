b9b2c3d32d4cb45bd13ad10848cd7044ae69dab1
Ricardo <Ricardo@Dell-RicHeinz>
2017-04-05 00:38:28 -0300

    1º commit do projeto

---
+++ src/java/br/edu/ifsul/controle/ControleCarro.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.controle;
+
+import br.edu.ifsul.dao.CarroDAO;
+import br.edu.ifsul.modelo.Carro;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Ricardo
+ */
+@ManagedBean(name = "controleCarro")
+@SessionScoped
+public class ControleCarro implements Serializable{
+    
+    private CarroDAO dao;
+    private Carro objeto;
+    
+    
+     public ControleCarro(){
+        dao = new CarroDAO();
+    }
+    
+    public String listar(){
+        return"/privado/carro/listar?faces-redirect=true";
+    }
+    
+    public String novo(){
+        objeto = new Carro();
+        return "formulario";
+    }
+    
+    public String salvar(){
+        if(dao.salvar(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+            return "listar";
+        }else{
+            Util.mensagemErro(dao.getMensagem());
+            return "formulario";
+        }
+    }
+    
+    public String cancelar(){
+        return "listar";
+    }
+    
+    public String editar(Integer id){
+        try{
+            objeto = dao.localizar(id);
+            return "formulario";
+        }catch(Exception e){
+            Util.mensagemErro("Erro ao recuperar objeto: "+Util.getMensagemErro(e));
+            return "listar";
+        }
+    }
+    
+    public void remover(Integer id){
+        objeto = dao.localizar(id);
+        if(dao.remover(objeto)){
+            Util.mensagemInformacao(dao.getMensagem());
+        }else{
+            Util.mensagemErro(dao.getMensagem());
+        }
+    }
+    
+
+    public CarroDAO getDao() {
+        return dao;
+    }
+
+    public void setDao(CarroDAO dao) {
+        this.dao = dao;
+    }
+
+    public Carro getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Carro objeto) {
+        this.objeto = objeto;
+    }
+    
+    
+    
+}
---
+++ src/java/br/edu/ifsul/dao/CarroDAO.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.dao;
+
+import br.edu.ifsul.jpa.EntityManagerUtil;
+import br.edu.ifsul.modelo.Carro;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class CarroDAO implements Serializable{
+    
+    private String mensagem = "";
+    private EntityManager em;
+    
+    public CarroDAO(){
+        em = EntityManagerUtil.getEntityManager();
+    }
+    
+    public List<Carro> getLista(){
+        return em.createQuery("from carro order by ano_fabricacao").getResultList();
+    }
+    
+    public boolean salvar(Carro obj){
+        try{
+            em.getTransaction().begin();
+            if(obj.getId() == null){
+                em.persist(obj);
+            }else{
+                em.merge(obj);
+            }
+            em.getTransaction().commit();
+            mensagem = "Objeto persistido com sucesso!";
+            return true;
+        }catch(Exception e){
+            if(em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            mensagem = "Erro ao persistir objeto: "+Util.getMensagemErro(e);
+            return false;
+        }
+    }
+    
+    public boolean remover(Carro obj){
+        try{
+            em.getTransaction().begin();
+            em.remove(obj);
+            em.getTransaction().commit();
+            mensagem = "Objeto removido com sucesso!";
+            return true;
+        }catch(Exception e){
+            if(em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            mensagem = "Erro ao remover objeto: "+Util.getMensagemErro(e);
+            return false;
+        }
+    }
+    
+    public Carro localizar(Integer id){
+        return em.find(Carro.class, id);
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+}
